6ef6df8ad6e541e966e5f8316c68e20e6e643724
Shira Amrany <shira.amrany@gmail.com>
2020-07-16 14:37:52 +0000

    create rectange and add shape to canvas

--- shapes.py
+++ shapes.py
@@ -1,5 +1,5 @@
 
-def new_canvas():
+def create_canvas():
 	"""Create a canvas of 10x10 represented by lists within a list"""
 	canvas = []
 
@@ -7,6 +7,7 @@ def new_canvas():
 		canvas.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 	return canvas
+
 
 def print_canvas(canvas):
 	"""Parses the data in the canvas and prints to the terminal"""
@@ -29,9 +30,57 @@ def clear_canvas(canvas):
 
 	return canvas
 
-def create_rectangle():
+
+
+def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
+	"""Draws a rectangle given coordinates and character on two-dimensional canvas"""
+
+	canvas_for_rectangle = create_canvas()
+	
+	for x in range(start_x, end_x + 1, 1):
+		for y in range(start_y, end_y + 1, 1):
+			canvas_for_rectangle[x][y] = fill_char
+
+	return canvas_for_rectangle
+
+
+
+def add_shape_to_canvas(shape, canvas):
+	""" """
+	#for each coordinate in canvas if rectangle not 0 for same coordinate, change canvas to rectangle character.
+	for x in range(len(shape)):	
+		for y in range(len(shape[x])):
+			if shape[x][y] != 0:
+				canvas[x][y] = shape[x][y]
+
+	return canvas
+
+
+def change_fill(current_shape):
+	""""""
+	pass
+
+
+def translate_position(current_shape, direction, units_of_movement):	
+	""""""
+# left, right, up, or down
+# axis which axis ('x' or 'y') should we translate the rectangle on? Translating on the X-axis will cause the rectangle to move left and right. Translating on the Y-axis will cause the rectangle to move up and down.
+# num is how much to move the rectangle. Negative numbers will cause the rectangle to shift left or down. Positive numbers will cause the rectangle to shift right or up.
 
 	pass
 
-canvas = new_canvas()
+# canvas = create_canvas()
+# print_canvas(canvas)
+
+
+# Rectangles can overlap with one another. The most recently added rectangle should appear on top of other rectangles. For example:
+# Do not render items out of bounds
+
+
+
+rect = create_rectangle(1, 1, 3, 3, "*")
+# print_canvas(rect)
+canvas = create_canvas()
+# print_canvas(canvas)
+add_shape_to_canvas(rect, canvas)
 print_canvas(canvas)
